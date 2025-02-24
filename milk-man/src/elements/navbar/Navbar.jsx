35be36fedb5fe97e762e6ff746491065ec3beba8
Naveen Kumar Sheokand <nksheokand9991@gmail.com>
2025-02-24 21:28:55 +0530

    Responsive navbar and background

--- milk-man/src/elements/navbar/Navbar.jsx
+++ milk-man/src/elements/navbar/Navbar.jsx
@@ -2,7 +2,14 @@ import "./navbar.css";
 import { Link } from "react-router-dom";
 import { useState } from "react";
 import logoUrl from "../../assests/logo.png";
-
+//   define data array which is collection of object
+  const data = [
+    { path: "/", name: "Home", id: 1, icon: "home" },
+    { path: "/Records", name: "Records", id: 2, icon: "file_open" },
+    { path: "/Entry", name: "Entry", id: 3, icon: "post_add" },
+    { path: "/Manage", name: "Manage", id: 4, icon: "folder_managed" },
+    { path: "/About", name: "About-us", id: 5, icon: "mail" },
+  ];
 function Logo() {
   return (
     <>
@@ -14,14 +21,7 @@ function Logo() {
 // define <Menu /> for small screen
 function Menu() {
   const [menuClass, setMenuClass] = useState(false);
-  //   define data array which is collection of object
-  const data = [
-    { path: "/", name: "Home", key: "1", icon: "home" },
-    { path: "/Records", name: "Records", key: "2", icon: "file_open" },
-    { path: "/Entry", name: "Entry", key: "3", icon: "post_add" },
-    { path: "/Manage", name: "Manage", key: "4", icon: "folder_managed" },
-    { path: "/About", name: "About-us", key: "5", icon: "mail" },
-  ];
+  
   // function to handle click event of menu
   function handleClick() {
     if (menuClass === false) {
@@ -31,46 +31,57 @@ function Menu() {
     }
   }
   return (
-    <>
-      <div className="menu">
-        <span className="material-symbols-outlined" onClick={handleClick}>
-          menu
-        </span>
-        <div className={menuClass ? "menuShow" : "menuClose"}>
-          <div>
-            <span className="material-symbols-outlined" onClick={handleClick}>
-              close
-            </span>
-          </div>
-          <nav>
-            <ul>
-              {data.map((person) => {
-                return (
-                  <>
-                    <li className="navItems" key={person.key}>
-                      
-                      <Link onClick={handleClick} className="navLink" to={person.path}><span className="material-symbols-outlined">
-                        {person.icon}
-                      </span>{person.name}</Link>
-                    </li>
-                  </>
-                );
-              })}
-            </ul>
-          </nav>
+    <div className="menu">
+      <span className="material-symbols-outlined" onClick={handleClick}>
+        menu
+      </span>
+      <div className={menuClass ? "menuShow" : "menuClose"}>
+        <div>
+          <span className="material-symbols-outlined" onClick={handleClick}>
+            close
+          </span>
         </div>
+        <nav>
+        <ul>
+          {data.map((person) => 
+            <li className="navItems" key={person.id}>
+              <span className="material-symbols-outlined">{person.icon}</span>
+              <Link onClick={handleClick} className="navLink" to={person.path}>
+                {person.name}
+              </Link>
+            </li>
+          )}
+        </ul>
+        </nav>
       </div>
-    </>
+    </div>
   );
 }
+// define <NavbarLong /> for big screen navbar
+function NavbarLong(){
+  return (<div className="longNav">
+    
+      <ul>
+        {data.map(person =>
+            <li className="longNavItems" key={person.id}> 
+              <Link className="longNavLink" to={person.path}>
+                {person.name}
+              </Link>
+            </li>
+          )}
+      </ul>
+    
 
+  </div>);
+}
 export default function NavbarLayout() {
   return (
-    <div>
+  
       <div className="navbar">
         <Logo />
         <Menu />
+        <NavbarLong />
       </div>
-    </div>
+    
   );
 }
