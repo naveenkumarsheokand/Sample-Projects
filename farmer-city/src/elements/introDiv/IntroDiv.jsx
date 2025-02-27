a9d599d07d9d42df4352a2934ea55018de3ffda6
Naveen Kumar Sheokand <nksheokand9991@gmail.com>
2025-02-27 21:50:26 +0530

    New element and correction

--- farmer-city/src/elements/introDiv/IntroDiv.jsx
+++ farmer-city/src/elements/introDiv/IntroDiv.jsx
@@ -1,6 +1,34 @@
-import introUrl from "../../assets/introImage.png"
-export default function IntroDiv(){
-    return <div className="introduction-div">
+import "./introDiv.css";
+import introUrl from "../../assets/introImage.png";
+import checkUrl from "../../assets/check.png";
+export default function IntroDiv() {
+  return (
+    <div className="introduction-div">
+      <div className="intro-div-itemA">
         <img id="introImg" src={introUrl} alt="Image" />
+      </div>
+      <div className="intro-div-itemB">
+        <p>OUR INTRODUCTION</p>
+        <h3>Pure Agriculture and Organic Form</h3>
+        <p>We’re Leader in Agriculture Market</p>
+        <p>
+          There are many variations of passages of available but the majority
+          have suffered alteration in some form, by injected humou or randomised
+          words even slightly believable.
+        </p>
+        <p>
+          <img src={checkUrl} />
+          Organic food contains more vitamins
+        </p>
+        <p>
+          <img src={checkUrl} />
+          Eat organic because supply meets demand
+        </p>
+        <p>
+          <img src={checkUrl} />
+          Eat organic because supply meets demand
+        </p>
+      </div>
     </div>
+  );
 }
